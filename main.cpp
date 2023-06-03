64a20f2402ac71b90b9418e668ff2fd2fcde11cb
DmitriiKor <Dmitriikor@gmail.com>
2023-06-03 22:05:50 +0300

    ***

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ int main(int argc, char const* argv[])
 	/**
 	 * @brief Объявление интерфейса
 	 */
-	Interface window;
+	//Interface window;
 	/**
 	 * @brief скрытие консоли
 	 */
@@ -122,68 +122,68 @@ int main(int argc, char const* argv[])
 	/**
 	 * @brief запуск тестовой функции
 	 */
-	window.test_nana();
-	window.ShowConsoleWindow();
-	window.st_diag();
+	//window.test_nana();
+	//window.ShowConsoleWindow();
+	//window.st_diag();
 
 	/**
  	* @brief запуск таймера
  	* 
  	*/
-	time_t t1 = clock();													//! @param time_t t1 - запуск таймера, засекаем время
+	//time_t t1 = clock();													//! @param time_t t1 - запуск таймера, засекаем время
 
-	Triangle_hi Fin_Triangle;
-	Fin_Triangle = Find_Triangle(window.point_arr, window.n_points);
-
-
-	Ray<Dot> copy_to_print;
-	if (Fin_Triangle.get_dot_counter() != 0)
-	{
-		Corner out_print;
-
-		out_print.insert(Fin_Triangle.get_point_array(), '*');
-
-		//  for (size_t i = 0; i < Fin_Triangle.size_point_array(); i++)
-		//	copy_to_print.add_to_back(Fin_Triangle.get_point(i));
-
-		out_print.insert(copy_to_print);
-		out_print.insert_line(Fin_Triangle.get_a(), Fin_Triangle.get_b(), '*');
-		out_print.insert_line(Fin_Triangle.get_b(), Fin_Triangle.get_c(), '*');
-		out_print.insert_line(Fin_Triangle.get_c(), Fin_Triangle.get_a(), '*');
-		out_print.insert_line(Fin_Triangle.get_a(), Fin_Triangle.get_a(), '+');
-		out_print.render_matrix();
-		out_print.print();
-		out_print.print_to_file();
-
-		Axys a_out_print;
-		a_out_print += out_print;
-		a_out_print.render_matrix();
-		a_out_print.print();
-		a_out_print.insert_line({ 10,10 }, { -10,-10 }, '*');
-		a_out_print.render_matrix();
-		a_out_print.print();
-		a_out_print.remove_line({ 10,10 }, { -10,-10 });
-		a_out_print.render_matrix();
-		a_out_print.print();
+	//Triangle_hi Fin_Triangle;
+	//Fin_Triangle = Find_Triangle(point_arr, n_points);
 
 
-		Direct_draw ttst;
-		ttst.insert_line({ 0,0 }, { 0,0 }, '&');
-		ttst.insert_line({ 1,1 }, { 1,1 }, '/');
-		ttst.insert_line({ 3,1 }, { 3, 1 }, '\\');
-		ttst.insert_line({ 0,10 }, { 10, 0 }, '&');
-		ttst += out_print;
-		ttst.render_matrix();
-		ttst.print();
-	}
+	//Ray<Dot> copy_to_print;
+	//if (Find_Triangle.get_dot_counter() != 0)
+	//{
+	//	Corner out_print;
 
-	/**
-	 * @brief запуск второго таймера
-	 */
-	time_t t2 = clock();
-	std::cout << "timer = \t" << double(t2 - t1) / CLOCKS_PER_SEC << std::endl;
+	//	out_print.insert(Fin_Triangle.get_point_array(), '*');
 
-	int AV2;
-	std::cin >> AV2;
+	//	//  for (size_t i = 0; i < Fin_Triangle.size_point_array(); i++)
+	//	//	copy_to_print.add_to_back(Fin_Triangle.get_point(i));
+
+	//	out_print.insert(copy_to_print);
+	//	out_print.insert_line(Fin_Triangle.get_a(), Fin_Triangle.get_b(), '*');
+	//	out_print.insert_line(Fin_Triangle.get_b(), Fin_Triangle.get_c(), '*');
+	//	out_print.insert_line(Fin_Triangle.get_c(), Fin_Triangle.get_a(), '*');
+	//	out_print.insert_line(Fin_Triangle.get_a(), Fin_Triangle.get_a(), '+');
+	//	out_print.render_matrix();
+	//	out_print.print();
+	//	out_print.print_to_file();
+
+	//	Axys a_out_print;
+	//	a_out_print += out_print;
+	//	a_out_print.render_matrix();
+	//	a_out_print.print();
+	//	a_out_print.insert_line({ 10,10 }, { -10,-10 }, '*');
+	//	a_out_print.render_matrix();
+	//	a_out_print.print();
+	//	a_out_print.remove_line({ 10,10 }, { -10,-10 });
+	//	a_out_print.render_matrix();
+	//	a_out_print.print();
+
+
+	//	Direct_draw ttst;
+	//	ttst.insert_line({ 0,0 }, { 0,0 }, '&');
+	//	ttst.insert_line({ 1,1 }, { 1,1 }, '/');
+	//	ttst.insert_line({ 3,1 }, { 3, 1 }, '\\');
+	//	ttst.insert_line({ 0,10 }, { 10, 0 }, '&');
+	//	ttst += out_print;
+	//	ttst.render_matrix();
+	//	ttst.print();
+	//}
+
+	///**
+	// * @brief запуск второго таймера
+	// */
+	//time_t t2 = clock();
+	//std::cout << "timer = \t" << double(t2 - t1) / CLOCKS_PER_SEC << std::endl;
+
+	//int AV2;
+	//std::cin >> AV2;
 	return 0;
 }
